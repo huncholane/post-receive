562b929d5f5f4d2f94bb9ba10eda2a30468ad184
huncholane <113503551+huncholane@users.noreply.github.com>
2023-01-01 11:19:26 -0800

    all automatic

--- startproject.sh
+++ startproject.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-$project=$HOME/$1
+project=$HOME/$1
 
 git init --bare $project
 cp post-receive $project/hooks/post-receive
+sed -i 's/PROJECT_NAME/'$1'/' $project/hooks/post-receive 
 chmod +x $project/hooks/post-receive
